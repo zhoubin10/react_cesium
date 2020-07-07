69df0f4bbec48aa44c92635280e33b441b8474ad
jiegiser <cjieouba@outlook.com>
2020-07-07 15:31:18 +0800

    Chore: 调整项目结果使用typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,13 @@
-/*
- * @Descripttion: 
- * @Author: jiegiser
- * @Date: 2020-04-09 15:34:34
- * @LastEditors: jiegiser
- * @LastEditTime: 2020-04-10 08:46:07
- */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'antd/dist/antd.css';
-import 'cesium/Widgets/widgets.css'
 import App from './App';
-import buildModuleUrl from "cesium/Core/buildModuleUrl";
 import * as serviceWorker from './serviceWorker';
-buildModuleUrl.setBaseUrl('./Source');
+
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
